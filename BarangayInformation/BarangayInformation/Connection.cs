a21ed86ed37d1ce758b8f311deed35b339564642
etiennewayne <etiennewayne@gmail.com>
2021-10-11 17:13:24 +0800

    update repo

---
+++ BarangayInformation/BarangayInformation/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace BarangayInformation
+{
+    class Connection
+    {
+
+        public static MySqlConnection con()
+        {
+            return new MySqlConnection("server=localhost; database=brgy_info;user=root;password=''");
+        }
+    }
+}
