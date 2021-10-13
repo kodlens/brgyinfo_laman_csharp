22831bd2a4dd188b5ebc32445e47d1bf8d78ccee
etiennewayne <etiennewayne@gmail.com>
2021-10-13 14:14:21 +0800

    update nationality

---
+++ BarangayInformation/BarangayInformation/Class/Nationality.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//use mysql dll
+using MySql.Data.MySqlClient;
+//add also the System.windows.forms so we can use DataGridView Object
+using System.Windows.Forms;
+
+
+namespace BarangayInformation.Class
+{
+    class Nationality
+    {
+        //declare always this 3 var
+        MySqlConnection con;
+        MySqlCommand cmd;
+        string query;
+
+        //property of Nationality Class
+        //usually your properties are the column name from database
+        public string nationality { set; get; }
+
+
+        public int save()
+        {
+            int i = 0; //instead void, i use int to add some remarks when inserting to database, default 0, means failed.
+
+            //instantitation of class will use the new keyword
+            con = Connection.con(); //another type of insantiation of object, the new keyword can be found in Connection.cs
+            con.Open(); //open the connection
+            query = "INSERT INTO nationalities SET nationality=?n"; //query for database
+            cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("?n", this.nationality); //you can use this keyword, or remove the this keyword
+            i = cmd.ExecuteNonQuery(); //cmd.executenonquery since the query is not a select statement, it is an insert statement
+            //assign variable i. if insert is successfull, it will return 1, else 0.
+            cmd.Dispose(); //dispose the cmd object variable to free memory
+            con.Close(); //close the connection , detached the connection from database to free connections
+            con.Dispose(); //usually garbage collector of the language will auto dispose objects but i prefer disposing manually to avoid any future problem
+            return i;
+
+
+            //KLARO EMJEEEH!!!
+        }
+
+        public void update(int id)
+        {
+            //we will change this void into int like save() method
+        }
+
+        public void delete(int id)
+        {
+            //we will change this void into int like save() method
+
+        }
+
+        //by calling the System.Windows.Form, we can use the DataGridView Object here
+        public void all(DataGridView grid)
+        {
+
+        }
+
+        //subject for changes, depends on how we implement the searching
+        public void find(string key)
+        {
+
+        }
+    }
+}
