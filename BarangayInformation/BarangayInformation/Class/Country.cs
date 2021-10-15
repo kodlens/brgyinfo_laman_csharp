33ba5a50ef2662f79876052e4705c93c4f47e3dc
mjtagaloguin <mjtagaloguin@gmail.com>
2021-10-15 15:50:20 -0700

    2 error haha

---
+++ BarangayInformation/BarangayInformation/Class/Country.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+//add also the System.windows.forms so we can use DataGridView Object
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace BarangayInformation.Class
+{
+    class Country
+    {
+        //declare always this 3 var
+        MySqlConnection con;
+        MySqlCommand cmd;
+        string query;
+
+        public String country { set; get; }
+        
+        public int save()
+        {
+            int i = 0;
+            con = Connection.con(); //another type of insantiation of object, the new keyword can be found in Connection.cs
+            con.Open(); //open the connection
+            query = "INSERT INTO countries SET country=?n"; //query for database // INSERT INTO tablename SET columname = 'value'
+            cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("?n", this.country); //you can use this keyword, or remove the this keyword
+            i = cmd.ExecuteNonQuery(); //cmd.executenonquery since the query is not a select statement, it is an insert statement
+            //assign variable i. if insert is successfull, it will return 1, else 0.
+            cmd.Dispose(); //dispose the cmd object variable to free memory
+            con.Close(); //close the connection , detached the connection from database to free connections
+            con.Dispose(); //usually garbage collector of the language will auto dispose objects but i prefer disposing manually to avoid any future problem
+            return i;
+        }
+        public int update(int id)
+        {
+            int i = 0;
+            //we will change this void into int like save() method
+            con = Connection.con();
+            con.Open();
+            query = "UPDATE countries SET country=?n WHERE country_id = ?id";
+            cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("?n", this.country);
+            cmd.Parameters.AddWithValue("?id", id);
+            i = cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+            con.Dispose();
+            return i;
+        }
+
+        public int delete(int id)
+        {
+            int i = 0;
+            //we will change this void into int like save() method
+            con = Connection.con();
+            con.Open();
+            query = "DELETE FROM countries WHERE country_id = ?id";
+            cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("?id", id);
+            i = cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+            con.Dispose();
+            return i;
+        }
+
+        public void TD(DataGridView grid, string key)
+        {
+            con= Connection.con();
+            con.Open();
+            query = "SELECT * FROM countries WHERE country LIKE ?key";
+            cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("?key", key + "%");
+
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adptr = new MySqlDataAdapter(cmd);
+            adptr.Fill(dt);
+
+            adptr.Dispose();
+            cmd.Dispose();
+            con.Close();
+            con.Dispose();
+
+            //assign data to grid
+            grid.AutoGenerateColumns = false;
+            grid.DataSource = dt;
+        }
+        public void getData(int id)
+        {
+            con = Connection.con();
+            con.Open();
+            query = "SELECT * FROM countries WHERE country_id = ?id";
+            cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("?id", id);
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adptr = new MySqlDataAdapter(cmd);
+            adptr.Fill(dt);
+
+            adptr.Dispose();
+            cmd.Dispose();
+            con.Close();
+            con.Dispose();
+
+            if (dt.Rows.Count > 0)
+            {
+                this.country = Convert.ToString(dt.Rows[0]["country"]);
+            }
+        }
+
+    }
+}
